811cd8cac157d4e8aa50261c66da426200f21d73
Zyre <zyre.dev@gmail.com>
2023-07-31 15:36:13 -0300

    ✨ feat: Add new navigation design

---
+++ src/components/Molecules/Details/index.tsx
@@ -0,0 +1,136 @@
+import React, { HTMLAttributes, ReactNode, useEffect, useRef } from "react";
+import style from './style.module.css'
+import { Container } from "../../Atoms/Container";
+
+interface Props extends HTMLAttributes<HTMLDetailsElement> {
+  children: ReactNode,
+  open: boolean
+}
+
+interface PropsSummary extends HTMLAttributes<HTMLElement> {
+  children: ReactNode
+}
+
+const Root: React.FC<Props> = (props) => {
+  const ref = useRef<HTMLDetailsElement>(null)
+
+  useEffect(() => {
+
+    const details = ref.current;
+    const summary = ref.current?.querySelector('summary')
+    const wrapper: HTMLUListElement | null | undefined = ref.current?.querySelector(`.${style.content}`)
+
+    let animation: null | Animation = null;
+    let isClosing = false;
+    let isExpanding = false;
+
+    const onAnimationFinish = (open: boolean) => {
+      if (details !== null && summary && wrapper) {
+        if (open === false) {
+          details.removeAttribute('open')
+        } else {
+          details.setAttribute("open", "")
+        }
+
+        animation = null
+        isClosing = false
+        isExpanding = false
+        details.style.height = details.style.overflow = ''
+      }
+    }
+
+    const expand = () => {
+      if (details !== null && summary && wrapper) {
+
+        isExpanding = true;
+
+        const startHeight = `${details.offsetHeight}px`
+        const endHeight = `${summary.offsetHeight + wrapper.offsetHeight}px`
+
+        if (animation) {
+          animation.cancel()
+        }
+
+        animation = details.animate({
+          height: [startHeight, endHeight]
+        }, {
+          duration: 400,
+          easing: 'ease-out'
+        })
+
+        animation.onfinish = () => onAnimationFinish(true)
+        animation.oncancel = () => isExpanding = false
+      }
+    }
+
+    const open = () => {
+      console.log('open', details, summary, wrapper)
+      if (details !== null && summary && wrapper) {
+
+        details.style.height = `${details?.offsetHeight}px`
+        details.setAttribute("open", "")
+
+        window.requestAnimationFrame(() => expand())
+      }
+    }
+
+
+    const shrink = () => {
+      if (details !== null && summary && wrapper) {
+
+        isClosing = true
+        const startHeight = `${details?.offsetHeight}px`
+        const endHeight = `${summary?.offsetHeight}px`
+
+        if (animation) {
+          animation.cancel()
+        }
+
+        animation = details.animate({
+          height: [startHeight, endHeight]
+        }, {
+          duration: 400,
+          easing: 'ease-out'
+        }
+        )
+
+        animation.onfinish = () => onAnimationFinish(false)
+        animation.oncancel = () => isClosing = false
+
+      }
+    }
+
+    const onClick = (e: MouseEvent) => {
+      e.preventDefault()
+      if (details && details !== null) {
+        details.style.overflow = 'hidden'
+        if (isClosing || !details.open) {
+          open()
+        } else if (isExpanding || details.open) {
+          shrink()
+        }
+      }
+    }
+
+    summary?.addEventListener('click', onClick)
+
+    return () => {
+      summary?.removeEventListener('click', onClick)
+    }
+  }, [])
+
+
+  return <details {...props} ref={ref} open={props.open}>{props.children}</details>
+};
+
+const Summary: React.FC<PropsSummary> = (props) => {
+  return <summary {...props}>{props.children}</summary>
+}
+
+const Content: React.FC<PropsSummary> = (props) => {
+  return <Container {...props} className={[props.className, style.content].join(" ")}>
+    {props.children}
+  </Container>
+}
+
+export const Details = Object.freeze({ Root, Summary, Content })
--- src/components/Molecules/ListNotes/index.tsx
+++ src/components/Molecules/ListNotes/index.tsx
@@ -1,23 +1,66 @@
-import { LuFileText } from 'react-icons/lu';
+import { LuChevronDown, LuChevronUp, LuFileText } from 'react-icons/lu';
 import style from './style.module.css';
 import { INote } from '../../../types/note';
+import React, { useCallback } from 'react';
+import { Details } from '../Details';
 
 interface ListNotesProps {
   notes: INote[],
   changeNote: (noteId: string) => void
 }
 
-export const ListNotes = ({ notes, changeNote }: ListNotesProps) => {
+interface ListProps {
+  title: string,
+  notes: INote[],
+  openNote: (noteId: string) => void
+}
+
+
+const List: React.FC<ListProps> = ({ title, notes, openNote }) => {
+
+  const callback = useCallback(openNote, [openNote])
+
   return (
-    <ul className={style.list}>
-      {notes.map(note => (
-        <li className={style.item} key={note.id}>
-          <a href="#" className={style['item-action']} onClick={() => changeNote(note.id)}>
-            <LuFileText />
-            <span> {note.title}</span>
-          </a>
-        </li>
-      ))}
-    </ul>
+    <Details.Root className={style.group} open={title === 'Local' && true}>
+      <Details.Summary className={style.title}>
+        {title}
+        <span className={style.control}><LuChevronUp /></span>
+      </Details.Summary>
+      <Details.Content>
+        <ul>
+          {notes.map(note => (
+            <li className={style.item} key={note.id}>
+              <a href="#" className={style['item-action']} onClick={() => callback(note.id)}>
+                <i><LuFileText /></i>
+                <span>{note.title}</span>
+              </a>
+            </li>
+          ))}
+        </ul>
+      </Details.Content>
+    </Details.Root>
+  )
+}
+
+export const ListNotes = ({ notes, changeNote }: ListNotesProps) => {
+
+  const filterNotes = (notesUnfiltred: INote[]) => {
+    const arr: [INote[], INote[]] = [[], []]
+
+    notesUnfiltred.map(n => {
+      if (n.site === window.location.hostname) return arr[0].push(n)
+      arr[1].push(n)
+    })
+
+    return arr
+  }
+
+  const [localNotes, globalNotes] = filterNotes(notes)
+
+  return (
+    <>
+      <List title="Local" openNote={changeNote} notes={localNotes} />
+      <List title="Global" openNote={changeNote} notes={globalNotes} />
+    </>
   )
 }
