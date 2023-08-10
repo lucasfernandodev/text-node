e5c0736c758ca88a5e4ce056c5d677733777ec00
Zyre <zyre.dev@gmail.com>
2023-08-10 10:33:15 -0300

    🐛 fix: Fixing problems that the font does not load

--- src/components/Atoms/Modal/index.tsx
+++ src/components/Atoms/Modal/index.tsx
@@ -5,10 +5,48 @@ interface Props extends ComponentProps<'section'> {
   children: ReactNode
 }
 
+
 const Modal: React.FC<Props> = ({ ...props }) => {
 
   const ref = useRef<HTMLElement>(null)
   useDragModal(ref.current)
+
+  const newStyle = `
+      @font-face{
+        font-family: 'Merriweather';
+        src: url("${chrome.runtime.getURL('/fonts/MerriweatherSans-Regular.ttf')}");
+        font-weight: 400;
+        font-style: normal
+      }
+
+      @font-face{
+        font-family: 'Merriweather';
+        src: url("${chrome.runtime.getURL('/fonts/MerriweatherSans-Italic.ttf')}");
+        font-weight: 400;
+        font-style: italic
+      }
+
+      @font-face{
+        font-family: 'Merriweather';
+         src: url("${chrome.runtime.getURL('/fonts/MerriweatherSans-Bold.ttf')}");
+        font-weight: 800;
+        font-style: normal
+      }
+
+      @font-face{
+        font-family: 'Merriweather';
+        src: url("${chrome.runtime.getURL('/fonts/MerriweatherSans-BoldItalic.ttf')}");
+        font-weight: 800;
+        font-style: italic
+      }
+
+      @font-face{
+        font-family: 'Merriweather';
+        src: url("${chrome.runtime.getURL('/fonts/MerriweatherSans-Medium.ttf')}");
+         font-weight: 500;
+        font-style: normal
+      }
+    `
 
   useEffect(() => {
     if (ref.current) {
@@ -18,6 +56,7 @@ const Modal: React.FC<Props> = ({ ...props }) => {
 
   return (
     <section ref={ref} aria-modal="true" role="dialog" tabIndex={-1} {...props}>
+      <style dangerouslySetInnerHTML={{ __html: newStyle }}></style>
       {props.children}
     </section>
   )
