4da086d92447a3dfc2b6b872385e6bd11d8ce890
Lucas Fernando <lucasfernando.dev@gmail.com>
2023-09-09 21:16:42 -0300

    🔧 chore: Change name for script

--- src/core/browser/popup.tsx
+++ src/core/browser/popup.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Popup } from '../../pages/Popup'
-
 import '../../style/colors.css';
 import '../../style/popup/style.css';
+
 const root = document.getElementById('root_popup') as Element
 
 ReactDOM.createRoot(root).render(
